f1a37092329038b85db9498fdec81d38a9f7cb32
Tehedo <sanjuantejedo@gmail.com>
2024-10-24 15:28:50 +0800

    updated homefrontend

--- app/src/main/java/com/egls/transactia/SearchFragment.java
+++ app/src/main/java/com/egls/transactia/SearchFragment.java
@@ -1,14 +1,18 @@
 package com.egls.transactia;
 
+import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 public class SearchFragment extends Fragment {
+
     public SearchFragment() {
         // Required empty public constructor
     }
@@ -16,6 +20,82 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        // Find Spinners by their IDs
+        Spinner spinner1 = view.findViewById(R.id.spinner1);
+        Spinner spinner2 = view.findViewById(R.id.spinner2);
+        Spinner spinner3 = view.findViewById(R.id.spinner3);
+
+        // Define options for the spinners
+        String[] spinnerOptions1 = {"NEED", "OFFER", "ALL"};
+        String[] spinnerOptions2 = {"FAVOR", "SKILL", "ALL"};
+        String[] spinnerOptions3 = {"BY REGION", "BY PROVINCE", "BY CITY/MUNICIPALITY"};
+
+        // Custom ArrayAdapter to set all text color to black for Spinner 1
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, spinnerOptions1) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view;
+                textView.setTextColor(Color.BLACK); // Set text color to black for selected item
+                return view;
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView textView = (TextView) view;
+                textView.setTextColor(Color.BLACK); // Set text color to black for dropdown items
+                return view;
+            }
+        };
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+        // Custom ArrayAdapter to set all text color to black for Spinner 2
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, spinnerOptions2) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view;
+                textView.setTextColor(Color.BLACK); // Set text color to black for selected item
+                return view;
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView textView = (TextView) view;
+                textView.setTextColor(Color.BLACK); // Set text color to black for dropdown items
+                return view;
+            }
+        };
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
+        // Custom ArrayAdapter to set all text color to black for Spinner 3
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, spinnerOptions3) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view;
+                textView.setTextColor(Color.BLACK); // Set text color to black for selected item
+                return view;
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView textView = (TextView) view;
+                textView.setTextColor(Color.BLACK); // Set text color to black for dropdown items
+                return view;
+            }
+        };
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+
+        return view;
     }
 }
