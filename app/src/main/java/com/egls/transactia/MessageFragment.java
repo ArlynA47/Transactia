3b6a99a7f1712dee514b23f80f4a98011f8be741
Tehedo <sanjuantejedo@gmail.com>
2024-11-08 21:57:49 +0800

    addition of message

--- app/src/main/java/com/egls/transactia/MessageFragment.java
+++ app/src/main/java/com/egls/transactia/MessageFragment.java
@@ -1,7 +1,9 @@
 package com.egls.transactia;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -16,6 +18,16 @@ public class MessageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_message, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_message, container, false);
+        ConstraintLayout trytoinbox = view.findViewById(R.id.trytoinbox);
+
+        trytoinbox.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), Inbox.class);  // Request is your target activity
+            startActivity(intent);
+        });
+
+
+return view;
     }
 }
