6b8f82064fd8bfb1d758293bb776e1ed6b7d0b24
ArlynA47 <arlynmusadaa47@gmail.com>
2024-11-11 21:03:25 +0800

    debug after adding search function

--- app/src/main/java/com/egls/transactia/Account_Settings.java
+++ app/src/main/java/com/egls/transactia/Account_Settings.java
@@ -58,9 +58,7 @@ public class Account_Settings extends AppCompatActivity {
         // Set click listener for passwordreset
         passwordreset.setOnClickListener(v -> {
             // Make the passpop visible and bring it to the front
-            pas                              spop.setVisibility(View.VISIBLE);
-            passpop.bringToFront(); // Bring the pop-up to the front
-            // Optionally, you can add animations here to make it appear nicely
+
         });
 
         // Set click listener for cancel2 button
