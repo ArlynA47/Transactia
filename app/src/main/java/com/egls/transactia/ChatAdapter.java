0eac45d1c96787d852fbf06e612374676994fdaf
ArlynA47 <arlynmusadaa47@gmail.com>
2024-11-28 20:48:15 +0800

    final

--- app/src/main/java/com/egls/transactia/ChatAdapter.java
+++ app/src/main/java/com/egls/transactia/ChatAdapter.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.egls.transactia.R;
 
 import java.util.List;
+import java.util.Locale;
 
 public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
     private Context context;
@@ -34,16 +35,29 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
     @Override
     public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
         Message message = messageList.get(position);
+        com.google.firebase.Timestamp timestamp = message.getTimestamp();
 
         if (message.getSenderId().equals(currentUserId)) {
             holder.sentMessage.setVisibility(View.VISIBLE);
             holder.receivedMessage.setVisibility(View.GONE);
             holder.sentMessage.setText(message.getMessageText());
+            holder.sentTimestamp.setVisibility(View.VISIBLE);
+            holder.sentTimestamp.setText(formatTimestamp(timestamp));
         } else {
             holder.receivedMessage.setVisibility(View.VISIBLE);
             holder.sentMessage.setVisibility(View.GONE);
             holder.receivedMessage.setText(message.getMessageText());
+            holder.receivedTimestamp.setVisibility(View.VISIBLE);
+            holder.receivedTimestamp.setText(formatTimestamp(timestamp));
         }
+    }
+
+    private String formatTimestamp(com.google.firebase.Timestamp timestamp) {
+        if (timestamp != null) {
+            java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("hh:mm a", Locale.getDefault());
+            return sdf.format(timestamp.toDate());
+        }
+        return "";
     }
 
     @Override
@@ -52,12 +66,14 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
     }
 
     public static class ChatViewHolder extends RecyclerView.ViewHolder {
-        TextView sentMessage, receivedMessage;
+        TextView sentMessage, receivedMessage, sentTimestamp, receivedTimestamp;
 
         public ChatViewHolder(@NonNull View itemView) {
             super(itemView);
             sentMessage = itemView.findViewById(R.id.sentMessage);
             receivedMessage = itemView.findViewById(R.id.receivedMessage);
+            sentTimestamp = itemView.findViewById(R.id.messageTimestamp1);
+            receivedTimestamp = itemView.findViewById(R.id.messageTimestamp2);
         }
     }
 }
